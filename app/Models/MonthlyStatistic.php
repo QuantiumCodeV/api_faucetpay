fb83e849a859d67ef40470e240a02d87daf72cfb
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-08 03:12:28 +0500

    fix

--- app/Models/MonthlyStatistic.php
+++ app/Models/MonthlyStatistic.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class MonthlyStatistic extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'coin',
+        'date',
+        'value',
+    ];
 }
