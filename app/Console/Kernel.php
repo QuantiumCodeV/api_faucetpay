cc4ebe9b5986a67aede582e4b8c2cbfb6e5484f5
QuantiumCodeV <quantiumcode@mail.ru>
2024-11-08 03:37:39 +0500

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // Запланируйте выполнение задания DailyInterestJob раз в день
-        $schedule->job(new \App\Jobs\DailyInterestJob)->everyTwoSeconds();
+        $schedule->job(new \App\Jobs\DailyInterestJob)->everyMinute();
     }
 
     protected function commands()
